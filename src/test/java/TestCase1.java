4c939224941b85c518b6c8766f6ab0025ed31f8b
Asif <asif@tagrem.com>
2018-05-14 12:28:22 +0530

    test commit

---
+++ src/test/java/TestCase1.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+    @Test
+    public void m1(){
+        System.out.println("Method 1");
+
+    }
+
+
+    @Test
+    public void m2(){
+        System.out.println("Method 2");
+    }
+
+}
